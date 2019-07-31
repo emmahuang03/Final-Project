bc5e5c2b83fe5c32da0e1db766eb984d677787f2
Apple <apple@Apples-MacBook-Pro-78.local>
2019-07-31 10:32:19 -0700

    Donate

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var submit = $('.submit');
 submit.on("click", enter);
 
 function enter(){
-  var names = $('.org').val();
+  var names = $('.names').val();
   var org1 = $('.org1').val();
   var org2 = $('.org2').val();
   var org3 = $('.org3').val();
@@ -11,11 +11,15 @@ function enter(){
   var don2 = $('.don2').val();
   var don3 = $('.don3').val();
   var answer = $('.answer');
-  if (names.length == 0) {
-    return false;
+  var body = $('body');
+
+  if(names.length == 0) {
+    answer.text("Something is missing. Please go back and fill it again.");
+  } else if ($('.org1').prop("checked") == false && $('.org2').prop("checked") == false && $('.org3').prop("checked") == false) {
+    answer.text("Something is missing. Please go back and fill it again.");
+  } else if ($('.don1').prop("checked") == false && $('.don2').prop("checked") == false && $('.don3').prop("checked") == false){
     answer.text("Something is missing. Please go back and fill it again.");
   } else {
-    return true;
-    answer.text("Thank you! We will contact you soon.");
+    body.text("Thank you! We will contact you soon.");
   }
 }
